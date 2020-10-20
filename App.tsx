080e699b2e616f85cfab5ac4873323dd4e9f2b14
Rafael Tavares <rafael_tcb@hotmail.com>
2020-10-20 10:21:55 -0300

    Fix lint errors and warnings

--- App.tsx
+++ App.tsx
@@ -26,7 +26,7 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-declare const global: {HermesInternal: null | {}};
+declare const global: { HermesInternal: null | {} };
 
 const App = () => {
   return (
@@ -46,8 +46,8 @@ const App = () => {
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
               <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-                screen and then come back to see your edits.
+                Edit App.tsx to change this screen and then come back to see
+                your edits.
               </Text>
             </View>
             <View style={styles.sectionContainer}>
@@ -101,9 +101,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '400',
     color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
   },
   footer: {
     color: Colors.dark,
