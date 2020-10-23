a06c549e07162db52168303c1ce3b406a3650a94
Rafael Tavares <rafael_tcb@hotmail.com>
2020-10-23 20:32:00 -0300

    Rename ESLint import group

--- .eslintrc.js
+++ .eslintrc.js
@@ -182,7 +182,7 @@ module.exports = {
           'module',
           '//services/',
           '//contexts/',
-          '//pages/',
+          '//screens/',
           '//components/',
           ['parent', 'sibling', 'index'],
         ],
